9e406ffc4cdf53db1727cdcb69717ae19273d079
Ariel Leonardo Castro <ariel24@ufl.edu>
2017-09-22 15:08:30 -0400

    Changed URLs once again

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -22,23 +22,23 @@ angular.module('listings').factory('Listings', ['$http',
       // }
 
         getAll: function() {
-        return $http.get('http://radiant-waters-19655.herokuapp.com/api/listings');
+        return $http.get('https://radiant-waters-19655.herokuapp.com/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://radiant-waters-19655.herokuapp.com/api/listings', listing);
+        return $http.post('https://radiant-waters-19655.herokuapp.com/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://radiant-waters-19655.herokuapp.com/api/listings/' + id);
+        return $http.get('https://radiant-waters-19655.herokuapp.com/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://radiant-waters-19655.herokuapp.com/api/listings/' + id, listing);
+        return $http.put('https://radiant-waters-19655.herokuapp.com/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://radiant-waters-19655.herokuapp.com/api/listings/' + id);
+        return $http.delete('https://radiant-waters-19655.herokuapp.com/api/listings/' + id);
       }
 
 
